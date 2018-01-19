b7139637a43f78f90b9460904b68cd798c8fb1e0
Jordan Reed <jordanr23@gmail.com>
2018-01-18 21:06:41 -0700

    hackathon ending result

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,35 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/query');
 
+var arr= ["https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif", "https://media.giphy.com/media/GnCc88zZhSVUc/giphy.gif", "https://media.giphy.com/media/YqvHYxcBDcfAs/giphy.gif", "https://media.giphy.com/media/R8RscTZV7Iy7m/giphy.gif",
+"https://media.giphy.com/media/tknTt9MXVeP84/giphy.gif", "https://media.giphy.com/media/vo6h46NUgExPy/giphy.gif"];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   db.getResolutions()
   .then(data => {
-    res.render('index', {title: 'RIP', titles: data});
+    res.render('index', {titles: data});
   })
 });
 
 router.get('/:id', function(req, res, next) {
   db.getResolution(req.params.id)
   .then(data => {
-    res.render('category', {title: 'RIP', titles: data});
+    res.render('category', {id: req.params.id, titles: data, gif: arr[req.params.id - 1]});
   })
 });
 
-router.put('/:id/:entry', function(req, res, next) {
-  db.vote(req.params.entry, req.body.vote)
-  .then(data => {
-    res.render('category', {title: 'RIP', titles: data});
-  })
-});
-
-router.post('/:id', function(req, res, next) {
-  req.body.votes = 0;
-  req.body.cat_id = req.params.id
+router.post('/add', function(req, res, next) {
   db.addEntry(req.body)
   .then(data => {
-    res.render('category', {title: 'RIP', titles: data});
+    res.redirect(`/${req.body.cat_id}`)
+  })
+});
+
+router.post('/vote/:id', function(req, res, next) {
+  db.vote(req.body.cat_id, req.body.vote++)
+  .then(data=>{
+    res.redirect(`/${req.body.cat_id}`)
   })
 });
 
